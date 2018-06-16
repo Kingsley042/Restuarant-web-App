36477d90b5cb5dd1da22091b61fb00b1bac5f5d0
Odo Kingsley Chigekwu <40233404+Kingsley042@users.noreply.github.com>
2018-06-16 05:49:29 +0100

    removed some wrong content

--- index.php
+++ index.php
@@ -125,12 +125,6 @@
 			<br>
 				We are a world-class restaurant that offer assorted varieties of foods ranging from our local foods to foreign foods, drinks, snacks, wines and all you can think of.
 
-			We started in the year Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 		</div>
 	</div>
 <!-- //skills -->
